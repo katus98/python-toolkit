16e9a9dc3752c207431293b6361314f485e14030
katus <sunkeran0818@outlook.com>
2021-10-26 11:30:02 +0800

    support transform coordinates

---
+++ geocoding/coordinates_transform.py
@@ -0,0 +1,66 @@
+import math
+
+x_pi = 3.14159265358979324 * 3000.0 / 180.0
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+ee = 0.00669342162296594323  # 扁率
+
+
+def gcj02_to_wgs84(lng, lat):
+    """
+    GCJ02(火星坐标系)转GPS84
+    :param lng:火星坐标系的经度
+    :param lat:火星坐标系纬度
+    :return:
+    """
+    if out_of_china(lng, lat):
+        return lng, lat
+    d_lat = transform_lat(lng - 105.0, lat - 35.0)
+    d_lng = transform_lng(lng - 105.0, lat - 35.0)
+    rad_lat = lat / 180.0 * pi
+    magic = math.sin(rad_lat)
+    magic = 1 - ee * magic * magic
+    sqrt_magic = math.sqrt(magic)
+    d_lat = (d_lat * 180.0) / ((a * (1 - ee)) / (magic * sqrt_magic) * pi)
+    d_lng = (d_lng * 180.0) / (a / sqrt_magic * math.cos(rad_lat) * pi)
+    mg_lat = lat + d_lat
+    mg_lng = lng + d_lng
+    return lng * 2 - mg_lng, lat * 2 - mg_lat
+
+
+def transform_lat(lng, lat):
+    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+          0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lat * pi) + 40.0 *
+            math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 *
+            math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+
+def transform_lng(lng, lat):
+    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+          0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lng * pi) + 40.0 *
+            math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 *
+            math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+
+def out_of_china(lng, lat):
+    """
+    判断是否在国内，不在国内不做偏移
+    :param lng:
+    :param lat:
+    :return:
+    """
+    if lng < 72.004 or lng > 137.8347:
+        return True
+    if lat < 0.8293 or lat > 55.8271:
+        return True
+    return False
--- geocoding/geocoder.py
+++ geocoding/geocoder.py
@@ -3,13 +3,14 @@ import time
 import multiprocessing as mp
 import urllib.request
 import pandas as pd
+import coordinates_transform as ct
 
 ak = ''
 
 
 def geo_coding():
     global ak
-    filename = r'data/海宁违法2020.csv'
+    filename = r'F:\data\form\hn-illegal\csv\hn_accidents.csv'
     dataset = pd.read_csv(filename, low_memory=False)
     pool_results = []
     num_pro = int(mp.cpu_count())
@@ -31,7 +32,7 @@ def geo_coding():
         results.append(pool_result.get())
     final_dataset = pd.concat(results)
     print('Result length:', len(final_dataset))
-    final_dataset.to_csv(r'data\result.csv', index=False, sep=',')
+    final_dataset.to_csv(r'F:\data\form\hn-illegal\csv\hn_accidents_geo_1.csv', index=False, sep=',')
 
 
 def single_geo_coding(processor_index, interval, dataset, l_ak):
@@ -39,19 +40,22 @@ def single_geo_coding(processor_index, interval, dataset, l_ak):
     for i, row in dataset.iterrows():
         if row['coding_level'] == '-':
             try:
-                url = 'https://api.map.baidu.com/geocoding/v3/?address={}&output=json&ak={}&city={}'.format(
+                url = 'https://api.map.baidu.com/geocoding/v3/?address={}&output=json&ak={}&city={}&ret_coordtype=gcj02ll'.format(
                     urllib.request.quote(row['address']), l_ak, urllib.request.quote('嘉兴市'))
                 request = urllib.request.Request(url)
                 page = urllib.request.urlopen(request, timeout=0.5)
                 res = json.load(page)
                 if res['status'] == 0:
                     result = res['result']
-                    lon, lat, comprehension, coding_level = result['location']['lng'], result['location']['lat'], result['comprehension'], result['level']
+                    lon, lat = ct.gcj02_to_wgs84(result['location']['lng'], result['location']['lat'])
+                    comprehension, coding_level = result['comprehension'], result['level']
                     dataset.iloc[i - processor_index * interval, -4] = lon
                     dataset.iloc[i - processor_index * interval, -3] = lat
                     dataset.iloc[i - processor_index * interval, -2] = comprehension
                     dataset.iloc[i - processor_index * interval, -1] = coding_level
                     time.sleep(0.02)
+                elif res['status'] == 302:
+                    break
                 else:
                     print('API-FAILED!', res['status'])
             except:
