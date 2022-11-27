bf9997a2bb6f12c0a0c5584499a4ad8df5dc42fd
katus <sunkeran0818@outlook.com>
2022-11-27 13:34:54 +0800

    更新逆地理编码

--- geocoding/reverse_geocoding.py
+++ geocoding/reverse_geocoding.py
@@ -1,6 +1,6 @@
 import json
 import urllib.request
-
+import time
 import pandas as pd
 
 ak = ''
@@ -8,30 +8,37 @@ ak = ''
 
 def reverse_geo_coding():
     global ak
-    filename = r'D:\Data\car_roads_points_Jinhua.csv'
+    filename = r'E:\graduation-project\data\roads_ori\car_roads_points_Jinhua.csv'
     dataset = pd.read_csv(filename, low_memory=False)
     dataset['name_new'] = ''
     count = 0
     for i, row in dataset.iterrows():
         try:
-            url = 'https://api.map.baidu.com/reverse_geocoding/v3/?ak={}&output=json&coordtype=wgs84ll&location={},{}&poi_types=道路'.format(
-                ak, row['lat'], row['lon'])
+            url = 'https://api.map.baidu.com/reverse_geocoding/v3/?ak={}&output=json&coordtype=wgs84ll&location={},{}&extensions_road={}'.format(
+                ak, row['lat'], row['lon'], 'true')
             request = urllib.request.Request(url)
             page = urllib.request.urlopen(request, timeout=0.5)
             res = json.load(page)
             if res['status'] == 0:
                 result = res['result']
-                addressComponent = result['addressComponent']
-                row['name_new'] = addressComponent['street']
-                count += 1
-                print(count)
+                roads = result['roads']
+                if len(roads) > 0:
+                    dataset.iloc[i, -1] = roads[0]['name']
+                    print(time.strftime('%Y-%m-%d %H:%M:%S'), 'finish', i)
+                    count += 1
+                else:
+                    dataset.iloc[i, -1] = '-'
+                    print(time.strftime('%Y-%m-%d %H:%M:%S'), 'no', i)
             elif res['status'] == 302:
                 break
             else:
-                print('API-FAILED!', res['status'])
+                print(time.strftime('%Y-%m-%d %H:%M:%S'), 'API-FAILED!', res['status'])
         except:
-            print('API-FAILED!', -1)
-    dataset.to_csv(r'D:\Data\car_roads_points_Jinhua_1.csv', index=False, sep=',')
+            print(time.strftime('%Y-%m-%d %H:%M:%S'), 'error', i)
+        finally:
+            time.sleep(0.1)
+    dataset.to_csv(r'E:\graduation-project\data\roads_ori\car_roads_points_Jinhua_res.csv', index=False, sep=',')
+    print(time.strftime('%Y-%m-%d %H:%M:%S'), 'All Finished!', count, '/', len(dataset))
 
 
 if __name__ == '__main__':
